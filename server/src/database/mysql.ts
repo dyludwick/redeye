1b67f571725da2759083e221d492f60340e28519
Dylan Ross Ludwick <dylan@devhominid.com>
2020-09-13 16:38:31 -0600

    convert JS to TS

--- server/src/database/mysql.ts
+++ server/src/database/mysql.ts
@@ -1,10 +1,11 @@
 import mysql from 'mysql';
-import logger from '../config/winston';
+import { logger } from '../config/winston';
+import { Database } from '../types';
 
-export default (database) => {
+export default (database: Database) => {
   const connection = mysql.createConnection({
     host: database.host || 'localhost',
-    port: database.port || '3306',
+    port: database.port || 3306,
     database: database.name,
     user: database.user || 'root',
     password: database.password
@@ -23,7 +24,7 @@ export default (database) => {
       });
     });
 
-  const getUser = (username) =>
+  const getUser = (username: string) =>
     new Promise((resolve, reject) => {
       connection.query(
         'SELECT * FROM `users` WHERE `email` = ?',
